d21ff54a1a51e6f116f961dbb677a3bc6634f532
Nhut Chau <chauminhnhut90@gmail.com>
2019-05-19 11:25:59 +0700

    Project Auto Training

---
+++ Training/src/main/java/Main.java
@@ -0,0 +1,46 @@
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.interactions.Actions;
+
+public class Main {
+
+
+    public static void main(String[] args) {
+        // Step 0 :  Config Webdriver
+        System.setProperty("webdriver.chrome.driver", "/Users/macbook/Downloads/chromedriver");
+
+        // Step 1
+        WebDriver webDriver = new ChromeDriver();
+        webDriver.get("http://demo.guru99.com/test/drag_drop.html");
+
+        Actions actions = new Actions(webDriver);
+
+        By bank = By.xpath("//a[contains(text(),'BANK')]");
+        WebElement elementSource = webDriver.findElement(bank);
+
+        By target = By.xpath("//div[@id='shoppingCart1']//li[@class='placeholder']");
+        WebElement elementTarget = webDriver.findElement(target);
+
+        actions.dragAndDrop(elementSource, elementTarget).perform();
+
+        sleep(50000);
+
+        webDriver.quit();
+    }
+
+    private static void sleep(long milisecond) {
+        try {
+            Thread.currentThread().sleep(milisecond);
+        } catch (Exception ex) {
+
+        }
+    }
+
+
+    //        webDriver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
+//        driver.manage().timeouts().pageLoadTimeout(TIME_OUT, TimeUnit.SECONDS);
+//        driver.manage().timeouts().setScriptTimeout(TIME_OUT, TimeUnit.SECONDS);
+
+}
