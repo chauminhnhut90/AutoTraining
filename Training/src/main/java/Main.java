7848dce32d29d81ee636b8ff463e4521218bd1d3
Nhut Chau <chauminhnhut90@gmail.com>
2019-05-19 11:53:00 +0700

    Add author

--- Training/src/main/java/Main.java
+++ Training/src/main/java/Main.java
@@ -4,8 +4,11 @@ import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.interactions.Actions;
 
+/**
+* @author nhutcm
+*
+* */
 public class Main {
-
 
     public static void main(String[] args) {
         // Step 0 :  Config Webdriver
